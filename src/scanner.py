d404e86bdc3c6577e47fe50e21daf7a20ab22a9a
ChangelingX <95889558+ChangelingX@users.noreply.github.com>
2022-04-15 14:40:26 +0000

    successfully detects hits in posts from book titles in database.

--- src/scanner.py
+++ src/scanner.py
@@ -1,21 +1,27 @@
 import praw
 import sqlite3
 
-r = praw.Reddit("bot1")
-con = sqlite3.connect('lightweaver.db')
-cur = con.cursor()
-subreddit = r.subreddit("lightweaver_bot")
+def main():
+    r = praw.Reddit("bot1")
+    con = sqlite3.connect('lightweaver.db')
+    cur = con.cursor()
+    subreddit = r.subreddit("lightweaver_bot")
 
-books = cur.execute('SELECT title FROM books')
+    cur.execute('SELECT title FROM books')
+    books = cur.fetchall()
 
-for submission in subreddit.hot(limit=5):
-    for book in books:
-        book = book[0]
-        print(book)
-        if book in submission.title:
-            print(f"Keyword found in {submission.title} {submission.id}")
-        if book in submission.selftext:
-            print(f"Keyword found in self text of {submission.title} {submission.id}")
-        for comment in submission.comments:
-            if book in comment.body:
-                print(f"Keyword found in comment {comment.body} {comment.id} of submission {submission.title} {submission.id}")
+    for submission in subreddit.top("all"):
+        print(submission.title)
+        for book in books:
+            book = str(book[0]).lower()
+            print(book)
+            if book in submission.title.lower():
+                print(f"Keyword found in {submission.title} {submission.id}")
+            if book in submission.selftext.lower():
+                print(f"Keyword found in self text of {submission.title} {submission.id}")
+            for comment in submission.comments:
+                if book in comment.body.lower():
+                    print(f"Keyword found in comment {comment.body} {comment.id} of submission {submission.title} {submission.id}")
+
+if __name__ == '__main__':
+    main()
