608af8c3805a7b25067bb4a9dde716c9791dae8d
ChangelingX <lee@brightstep.me>
2022-04-27 19:42:12 +0000

    created new scanner2.py class to refactor old scanner into more refined state

--- src/scanner.py
+++ src/scanner.py
@@ -12,20 +12,17 @@ def main():
     books = cur.fetchall()
 
     for submission in subreddit.top("all"):
-        #print(submission.title)
         for book in books:
+            books_to_post = []
             book = str(book[0]).lower()
-            #print(book)
             if book in submission.title.lower():
-                #print(f"Keyword found in {submission.title} {submission.id}")
-                poster.post_comment(0,submission.id, book)
+                books_to_post.append(book) 
+                poster.post_comment(submission.id, books_to_post)        
             if book in submission.selftext.lower():
-                #print(f"Keyword found in self text of {submission.title} {submission.id}")
-                poster.post_comment(0,submission.id, book)
+                poster.post_comment(submission.id, book)
             for comment in submission.comments:
                 if book in comment.body.lower():
-                    #print(f"Keyword found in comment {comment.body} {comment.id} of submission {submission.title} {submission.id}")
-                    poster.post_comment(1 ,comment.id, book)
+                    poster.post_comment(comment.id, book)
 
 if __name__ == '__main__':
     main()
