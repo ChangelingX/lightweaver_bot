7651b5ce11292c01c6776c8429ff55e5b87944ab
ChangelingX <lee@brightstep.me>
2022-04-28 23:21:58 +0000

    created logic to allow users to opt themselves into this bot

--- lightweaverdb.sql
+++ lightweaverdb.sql
@@ -5,4 +5,6 @@ INSERT INTO books VALUES(1,'Opening the Hand of Thought','Kosho Uchiyama','97808
 INSERT INTO books VALUES(2,'You Are The One You''ve Been Waiting For','Richard C Schwartz','9780615249322','https://ifs-institute.com/store/37','In this book, Richard Schwartz, the developer of the Internal Family Systems Model, applies the IFS Model to the topic of intimate relationships in an engaging, understandable, and personal style. Therapists and lay people alike will find this book to be an insightful exploration of how cultivating a relationship with the Self—the wise center of clarity, calmness, and compassion in each of us—creates the foundation for courageous love and resilient intimacy: the capacity to sustain and nourish a healthy intimate relationship. Self-leadership also allows us to embrace our partner''s feedback and use it to discover aspects of ourselves that seek healing. The book includes user-friendly exercises to facilitate learning.');
 CREATE TABLE replied_entries (id integer NOT NULL, reddit_id TEXT NOT NULL);
 INSERT INTO replied_entries (id, reddit_id) VALUES (0,"dummy");
+CREATE TABLE opted_in_users (id integer NOT NULL, reddit_username TEXT NOT NULL);
+INSERT INTO opted_in_users (id, reddit_username) VALUES (0, "dummy");
 COMMIT;
